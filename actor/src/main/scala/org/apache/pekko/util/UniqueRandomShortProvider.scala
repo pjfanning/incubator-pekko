514ac493c3d530aea16f86d702b7d2ed358229e1
PJ Fanning <pjfanning@users.noreply.github.com>
2023-06-15 19:26:52 +0100

    make ID generator thread safe

--- actor/src/main/scala/org/apache/pekko/util/UniqueRandomShortProvider.scala
+++ actor/src/main/scala/org/apache/pekko/util/UniqueRandomShortProvider.scala
@@ -10,8 +10,10 @@
 package org.apache.pekko.util
 
 import org.apache.pekko.annotation.InternalApi
+import org.apache.pekko.io.dns.IdGenerator
 
 import java.security.SecureRandom
+import java.util.concurrent.{ Semaphore, TimeUnit }
 
 /**
  * An efficient collision free threadsafe generator of pseudo random
@@ -27,77 +29,33 @@ import java.security.SecureRandom
 
 @InternalApi
 private[pekko] class UniqueRandomShortProvider {
-  import UniqueRandomShortProvider._
-  val r = new SecureRandom()
-
-  private var index = r.nextLong
-  private var increment = r.nextLong
+  private val lockTimeoutSeconds = 10
+  private val lock = new Semaphore(1)
+  private val secureRandom = new SecureRandom()
+  private var index = secureRandom.nextLong()
+  private var increment = secureRandom.nextLong()
   private var count = 1L
-  private var limit = 0
-  private val buffer: Array[Short] = new Array[Short](FillerLimit)
-  private val fillerOffset = 0
-  resetBuffer()
 
-  private def resetBuffer(): Unit = {
-    System.arraycopy(Filler, fillerOffset, buffer, 0, buffer.length)
-    limit = buffer.length
-  }
-  def nextId(): Short = {
-    if (limit == 0)
-      resetBuffer()
+  final def nextId(): Short = {
+    if (lock.tryAcquire(lockTimeoutSeconds, TimeUnit.SECONDS)) {
+      try {
+        val result = (0xFFFFFFFF & index).asInstanceOf[Short]
+        index -= increment
 
-    val idx = UniqueRandomShortProvider.mod(index, limit)
-    limit = limit - 1
-    // Update index and handle wrapping
-    index -= increment
-    // Incorporate the counter into the increment to create a
-    // tetrahedral number additional term, and handle wrapping.
-    increment -= {
-      count += 1
-      count - 1
+        // Incorporate the counter into the increment to create a
+        // tetrahedral number additional term.
+        increment -= {
+          count += 1
+          count - 1
+        }
+
+        result
+      } finally {
+        lock.release()
+      }
+    } else {
+      // failover to using a secure random if we can't acquire the lock
+      IdGenerator.random(secureRandom).nextId()
     }
-    val result = buffer(idx)
-    val src = idx + 1
-    if (src < buffer.length) {
-      val len = buffer.length - src
-      System.arraycopy(buffer, src, buffer, idx, len)
-    }
-    result
-  }
-}
-
-@InternalApi
-private[pekko] object UniqueRandomShortProvider {
-  private val FillerLimit = 0x10000
-
-  // This is lazy in order to avoid allocation unless someone
-  // actually calls `UniqueRandomShortProvider.nextId()` since
-  // MaxLimit is actually of non negligible size.
-  private lazy val Filler = {
-    val a = new Array[Short](FillerLimit)
-    var i = 1
-    while (i < FillerLimit) {
-      a(i) = i.toShort
-      i += 1
-    }
-    a
-  }
-
-  /**
-   * Performs a modulus calculation on an unsigned long and an integer divisor.
-   *
-   * @param dividend a unsigned long value to calculate the modulus of.
-   * @param divisor the divisor for the modulus calculation.
-   * @return the remainder or modulus value.
-   */
-  private def mod(dividend: Long, divisor: Int): Int = {
-    // See Hacker's Delight (2nd ed), section 9.3.
-    // Assume divisor is positive.
-    // Divide half the unsigned number and then double the quotient result.
-    val quotient = (dividend >>> 1) / divisor << 1
-    val remainder = dividend - quotient * divisor
-    // remainder in [0, 2 * divisor)
-    (if (remainder >= divisor) remainder - divisor
-     else remainder).toInt
   }
 }
