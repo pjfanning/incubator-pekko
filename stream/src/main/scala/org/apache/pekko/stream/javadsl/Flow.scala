5fac4aeffe727c7716cf06eaa85c469337d44c32
PJ Fanning <pjfanning@users.noreply.github.com>
2023-08-16 23:20:08 +0100

    Update Flow.scala

--- stream/src/main/scala/org/apache/pekko/stream/javadsl/Flow.scala
+++ stream/src/main/scala/org/apache/pekko/stream/javadsl/Flow.scala
@@ -839,9 +839,9 @@ final class Flow[In, Out, Mat](delegate: scaladsl.Flow[In, Out, Mat]) extends Gr
    * @since 1.1.0
    */
   def mapAsyncPartitioned[T, P](parallelism: Int,
-                                bufferSize: Int,
-                                extractPartition: function.Function[Out, P],
-                                 f: function.Function2[Out, P, CompletionStage[T]]): Flow[In, T, Mat] = {
+      bufferSize: Int,
+      extractPartition: function.Function[Out, P],
+      f: function.Function2[Out, P, CompletionStage[T]]): Flow[In, T, Mat] = {
     MapAsyncPartition.mapFlowAsyncPartition(delegate, parallelism, bufferSize)(extractPartition(_))(f(_, _).asScala)
       .asJava
   }
